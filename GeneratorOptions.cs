e5edc78e87ff91af0515d3ec64095a85ddaf9ce5
AraHaan <seandhunt_7@yahoo.com>
2021-03-26 23:06:48 -0400

    Removed default values from GeneratorOptions.

--- GeneratorOptions.cs
+++ GeneratorOptions.cs
@@ -9,12 +9,12 @@
         /// Gets the type to use to apply the attribute to embed the git information in that is within the assembly it is being applied to.
         /// </summary>
         /// <value>The type to use to apply the attribute to embed the git information in that is within the assembly it is being applied to.</value>
-        public string? AssemblyType { get; init; } = null;
+        public string? AssemblyType { get; init; }
 
         /// <summary>
         /// Gets if the type specified in AssemblyType is a generic type, by default this is set to false to indicate that the type is not a generic type.
         /// </summary>
         /// <value>If the type specified in AssemblyType is a generic type, by default this is set to false to indicate that the type is not a generic type.</value>
-        public bool IsGeneric { get; init; } = false;
+        public bool IsGeneric { get; init; }
     }
 }
