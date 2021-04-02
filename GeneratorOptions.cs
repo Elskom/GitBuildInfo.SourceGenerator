50b70b9a610cac9bf75d70f8f649aebe51f124f5
AraHaan <seandhunt_7@yahoo.com>
2021-04-01 21:33:18 -0400

    Fixed generating sources.
    Signed-off-by: AraHaan <seandhunt_7@yahoo.com>

--- GeneratorOptions.cs
+++ GeneratorOptions.cs
@@ -1,20 +1,13 @@
 ﻿namespace GitBuildInfo.SourceGenerator
 {
-    /// <summary>
-    /// Describes the options that feed into code generation.
-    /// </summary>
+    using System.Text.Json.Serialization;
+
     public record GeneratorOptions
     {
-        /// <summary>
-        /// Gets the type to use to apply the attribute to embed the git information in that is within the assembly it is being applied to.
-        /// </summary>
-        /// <value>The type to use to apply the attribute to embed the git information in that is within the assembly it is being applied to.</value>
+        [JsonPropertyName("AssemblyType")]
         public string? AssemblyType { get; set; }
 
-        /// <summary>
-        /// Gets if the type specified in AssemblyType is a generic type, by default this is set to false to indicate that the type is not a generic type.
-        /// </summary>
-        /// <value>If the type specified in AssemblyType is a generic type, by default this is set to false to indicate that the type is not a generic type.</value>
+        [JsonPropertyName("IsGeneric")]
         public bool IsGeneric { get; set; }
     }
 }
---
+++ GitInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace GitBuildInfo.SourceGenerator
+{
+    using System.Text.Json.Serialization;
+
+    public record GitInfo
+    {
+        [JsonPropertyName("GitHead")]
+        public string? GitHead { get; set; }
+
+        [JsonPropertyName("CommitHash")]
+        public string? CommitHash { get; set; }
+
+        [JsonPropertyName("GitBranch")]
+        public string? GitBranch { get; set; }
+    }
+}
