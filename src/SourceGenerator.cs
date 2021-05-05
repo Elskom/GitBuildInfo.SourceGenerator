56c83d9d06f6fcd71696b9cde1edad4c6c2e0282
AraHaan <seandhunt_7@yahoo.com>
2021-05-05 15:46:47 -0400

    Restructured Repository.
    Signed-off-by: AraHaan <seandhunt_7@yahoo.com>

---
+++ src/SourceGenerator.cs
@@ -0,0 +1,49 @@
+﻿namespace GitBuildInfo.SourceGenerator
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.Text;
+
+    /// <summary>
+    /// Source Generator for dumping git build information into a assembly level attribute on the compilation.
+    /// </summary>
+    [Generator]
+    public class SourceGenerator : ISourceGenerator
+    {
+        /// <inheritdoc/>
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // Source Generators do not need to fill this in.
+        }
+
+        /// <inheritdoc/>
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (context.Compilation is not CSharpCompilation)
+            {
+                return;
+            }
+
+            var gitBuildInfoJsonFile = context.AdditionalFiles
+                .FirstOrDefault(af => string.Equals(Path.GetFileName(af.Path), "GitBuildInfo.json", StringComparison.OrdinalIgnoreCase));
+            var gitInfoJsonFile = context.AdditionalFiles
+                .FirstOrDefault(af => string.Equals(Path.GetFileName(af.Path), "GitInfo.json", StringComparison.OrdinalIgnoreCase));
+            if (gitBuildInfoJsonFile is null || gitInfoJsonFile is null)
+            {
+                return;
+            }
+
+            context.AddSource(
+                "GitAssemblyInfo.g.cs",
+                SourceText.From(
+                    Generator.CreateAndGenerateCode(
+                        gitBuildInfoJsonFile.GetText(context.CancellationToken)!.ToString(),
+                        gitInfoJsonFile.GetText(context.CancellationToken)!.ToString()),
+                    Encoding.UTF8));
+        }
+    }
+}
